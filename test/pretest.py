4ade8fb96746144b89ae961a829f5c4727a11279
41171119H <143977676+41171119H@users.noreply.github.com>
2024-02-20 21:18:44 +0800

    Update pretest.py
    changing the path

--- test/pretest.py
+++ test/pretest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import csv
 
-data = pd.read_csv("C:\\Users\\PL_TAHRD\\test\\adult.csv")
+data = pd.read_csv("https://github.com/41171119H/PL_TAHRD/blob/main/test/adult.csv")
 #data.head(51)
 data
